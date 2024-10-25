3b1dcfc8207002415f96ce7c2fecfb7104e6ee7d
udechukwu destiny kelechi <udechukwudestinykelechi945@gmail.com>
2024-10-25 13:57:01 +0100

    start project and ask questions

---
+++ script.js
@@ -0,0 +1,16 @@
+// create a variable for the questions and their answers
+const questions = [
+    {
+        questions: 'whats the tallest moutain in the world?',
+        answer: 'everest'
+    },
+    {
+        questions: 'the I in IGHub stands for what?',
+        answer: 'innovation'
+    },
+    {
+        questions: 'what food ingredient ehances taste?',
+        answer: 'salt'
+    }
+]
+
